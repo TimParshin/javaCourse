91f49b9c986552ee0993d286160307a439db2934
Tim <parshintn@gmail.com>
2020-04-19 08:50:00 +0700

    Unit 2 hw implementation without lombok: Implemented fire sword

---
+++ unit2/homework/src/main/java/pro/it/sis/javacourse/FlamingAsphaltSword.java
@@ -0,0 +1,14 @@
+package pro.it.sis.javacourse;
+
+public class FlamingAsphaltSword extends Weapon {
+
+    private int physicalDamage = 100;
+    private int fireDamage = 50;
+
+    private Damage givenDamage = new Damage()
+            .setPhysicalDamage(this.physicalDamage)
+            .setIceDamage(this.fireDamage);
+
+    @Override
+    public Damage getGivenDamage() {return this.givenDamage;}
+}
